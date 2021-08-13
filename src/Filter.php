06333cb3a0dec26e79ec58221ce22bca46ec884b
Vladimir Savritsky <vladimir.savritsky@gmail.com>
2021-08-13 11:03:52 +0300

    first commit

---
+++ src/Filter.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace BitrixFilterBuilder;
+
+class Filter
+{
+    protected $filter = [];
+
+    public static function create()
+    {
+        return new Filter();
+    }
+
+    public function eq($field, $value): self
+    {
+        $this->filter['=' . $field] = $value;
+
+        return $this;
+    }
+
+    public function neq($field, $value): self
+    {
+        $this->filter['!' . $field] = $value;
+
+        return $this;
+    }
+
+    public function like($field, $value): self
+    {
+        $this->filter['=%' . $field] = $value;
+
+        return $this;
+    }
+
+    public function isNull($field): self
+    {
+        $this->filter[$field] = false;
+
+        return $this;
+    }
+
+    public function isNotNull($field): self
+    {
+        $this->filter['!' . $field] = false;
+
+        return $this;
+    }
+
+    public function in($field, $values): self
+    {
+        $this->filter[$field] = (array)$values;
+
+        return $this;
+    }
+
+    public function notIn($field, $values): self
+    {
+        $this->filter['!' . $field] = (array)$values;
+
+        return $this;
+    }
+
+    public function between($field, $min, $max): self
+    {
+        $this->filter['>=' . $field] = $min;
+        $this->filter['<=' . $field] = $max;
+
+        return $this;
+    }
+
+    public function addOrFilter(Filter $filter): self
+    {
+        $this->filter[] = [
+            'LOGIC' => 'OR',
+            $filter->getResult()
+        ];
+
+        return $this;
+    }
+
+    public function getResult()
+    {
+        return $this->filter;
+    }
+}
