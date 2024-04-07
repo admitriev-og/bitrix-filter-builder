e4924ba41c8e0e872ba91558263cbe65940bab4a
Vladimir Savritsky <vladimir.savritsky@gmail.com>
2024-04-07 19:07:40 +0300

    fix error gte lte, add gt lt, implement JsonSerializable

--- src/Filter.php
+++ src/Filter.php
@@ -2,18 +2,18 @@
 
 namespace BitrixFilterBuilder;
 
-class Filter
+class Filter implements \JsonSerializable
 {
     protected $filter = [];
 
-    public static function create()
+    public static function create(): Filter
     {
         return new Filter();
     }
 
     public function eq($field, $value): self
     {
-        $this->filter[$field] = $value;
+        $this->filter['=' . $field] = $value;
 
         return $this;
     }
@@ -70,21 +70,35 @@ class Filter
 
     public function lte($field, $value): self
     {
-        $this->filter['<=' . $field] = $min;
+        $this->filter['<=' . $field] = $value;
 
         return $this;
     }
 
     public function gte($field, $value): self
     {
-        $this->filter['>=' . $field] = $min;
+        $this->filter['>' . $field] = $value;
+
+        return $this;
+    }
+
+    public function lt($field, $value): self
+    {
+        $this->filter['<' . $field] = $value;
+
+        return $this;
+    }
+
+    public function gt($field, $value): self
+    {
+        $this->filter['>=' . $field] = $value;
 
         return $this;
     }
 
     public function addOrFilter(Filter $filter): self
     {
-        $this->filter[] = array_merge([ 'LOGIC' => 'OR'], $filter->getResult());
+        $this->filter[] = array_merge(['LOGIC' => 'OR'], $filter->getResult());
 
         return $this;
     }
@@ -92,5 +106,15 @@ class Filter
     public function getResult()
     {
         return $this->filter;
+    }
+
+    public function toArray(): array
+    {
+        return $this->filter;
+    }
+
+    public function jsonSerialize()
+    {
+        return $this->toArray();
     }
 }
